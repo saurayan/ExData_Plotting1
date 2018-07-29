c756b97467f0306a89e26aa893f8de418bde72d2
Saurayan Chaki <saurayan@gmail.com>
2018-07-28 23:00:42 -0500

    Updating plot4.R

--- Plot4.R
+++ Plot4.R
@@ -45,3 +45,4 @@ lines(tbl_sub$datetime, tbl_sub$Sub_metering_3,type="l", col= "blue")
 legend(c("topright"), c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty= 1, lwd=2, col = c("black", "red", "blue"))
 with(tbl_sub, plot(datetime, Global_reactive_power, type="l", xlab="datetime", ylab="Global_reactive_power"))
 dev.off()
+
